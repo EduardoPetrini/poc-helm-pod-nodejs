da99db2f18699c7a56cc57ec27d424fbb48fc7e8
EduardoPetrini <eduardo.petrini@outlook.com>
2022-07-17 11:09:52 -0300

    feat: first commit - structure

---
+++ app/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log('Hello World!');
+  res.send('Hello World!');
+});
+
+module.exports = app;
